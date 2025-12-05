068434b9b6c14b579b160c1442f5fdaae68b65b6
Ludhfi <ludhfiansyah@gmail.com>
2025-12-05 16:02:27 +0000

    submit Numeric Operator assignment

---
+++ assignments/02-numeric-operator/2.js
@@ -0,0 +1,7 @@
+const operand1 = 9.5 * 4.5
+const operand2 = 2.5 * 3
+const operand3 = 45.5 - 3.5
+
+console.log(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+
+console.log((operand1 - operand2) / operand3);
