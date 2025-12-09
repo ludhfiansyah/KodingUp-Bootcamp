f982a7887a20e178e08c85b9b3ae5d78c3f5f518
Ludhfi <ludhfiansyah@gmail.com>
2025-12-09 05:35:15 +0000

    update new line in file 1.js

--- assignments/03-reading-input-from-console/1.js
+++ assignments/03-reading-input-from-console/1.js
@@ -3,6 +3,7 @@ const readline = require ('readline-sync');
 const firstNumber = +readline.question('Enter first number: ');
 const secondNumber = +readline.question('Enter second number: ');
 const thirdNumber = +readline.question('Enter third number: ');
+
 const average = (firstNumber + secondNumber + thirdNumber) / 3;
 
 console.log('');
