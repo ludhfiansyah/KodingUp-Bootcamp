3ffb25ac18287d2ee73f5ce76a7c7b83a36b45ff
Ludhfi <ludhfiansyah@gmail.com>
2025-12-09 23:55:07 +0000

    Update comment for assignment 03-reading-input-from-console/2.js

--- assignments/03-reading-input-from-console/2.js
+++ assignments/03-reading-input-from-console/2.js
@@ -3,7 +3,7 @@ const readline = require ('readline-sync');
 // Get user input
 const inputMiles = +readline.question('Enter miles: ');
 
-// Convert miles to kilometers calculation and rounds it to 5 decimal
+// Miles to kilometers convertion and rounds it to 5 decimal
 const kilometers = inputMiles * 1.609344;
 const km = Math.round(kilometers * 100000) / 100000;
 
