46be1d438fcdc17bc0af52076d1584a7b5b36faf
Ludhfi <ludhfiansyah@gmail.com>
2025-12-09 05:39:38 +0000

    update code parseInt usage in 05-determine-whether-a-number-is-even-or-odd

--- assignments/05-determine-whether-a-number-is-even-or-odd/1.js
+++ assignments/05-determine-whether-a-number-is-even-or-odd/1.js
@@ -1,9 +1,9 @@
 const readline = require ('readline-sync');
 
-const number = +readline.question('Enter a number: ');
+const number = parseInt(readline.question('Enter a number: '));
 
-if (parseInt(number) % 2 === 0) {
-    console.log(parseInt(number) + ' is even');
+if (number % 2 === 0) {
+    console.log(number + ' is even');
 } else {
-    console.log(parseInt(number) + ' is odd');
+    console.log(number + ' is odd');
 }
