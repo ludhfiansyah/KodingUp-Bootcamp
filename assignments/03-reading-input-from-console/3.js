f5cb362a5d232ff5891803502dd456ca1e4a58df
Ludhfi <ludhfiansyah@gmail.com>
2025-12-09 13:54:29 +0000

    update 03-reading-input-from-console file 3.js to use Math.trunc()

--- assignments/03-reading-input-from-console/3.js
+++ assignments/03-reading-input-from-console/3.js
@@ -1,7 +1,7 @@
 const readline = require ('readline-sync');
 
 const inputSeconds = +readline.question('Enter seconds: ');
-const minutes = parseInt(inputSeconds / 60);
+const minutes = Math.trunc(inputSeconds / 60);
 const second = inputSeconds % 60;
 
 console.log(inputSeconds +' second is '+ minutes + ' minutes and ' + second + ' seconds');
