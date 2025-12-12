5da356f2e0ff5f25a7931d095a4568904c8bc6e4
Ludhfi <ludhfiansyah@gmail.com>
2025-12-12 14:55:03 +0000

    REVISION file 2.js to use GLOBA const variable

--- assignments/03-reading-input-from-console/2.js
+++ assignments/03-reading-input-from-console/2.js
@@ -1,12 +1,16 @@
 const readline = require ('readline-sync');
 
+// REVISION - Adding GLOBAL CONST for Kilometers_per_Miles value
+const KILOMETERS_PER_MILES = 1.609344;
+
 // Get user input
 const inputMiles = +readline.question('Enter miles: ');
 
-// Miles to kilometers convertion and rounds it to 5 decimal
-const kilometers = inputMiles * 1.609344;
-const km = Math.round(kilometers * 100000) / 100000;
+// Convert miles to kilometers (rounded to 5 decimals)
+const kilometers = Math.round(inputMiles * KILOMETERS_PER_MILES * 100000) / 100000;
+
+// const km = Math.round(kilometers * 100000) / 100000; REVISION - prev code
 
 // Print output
-console.log('');
-console.log(inputMiles +' miles is equal to ' + km +' kilometers');
+// console.log(''); - REVISION - not to use this to print new line
+console.log('\n' + inputMiles +' miles is equal to ' + kilometers +' kilometers'); // REVISION -  use \n to print new line
