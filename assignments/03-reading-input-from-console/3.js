ba2809b3df70bb3d4a3fc6f11a5edd2a7e8cfabd
Ludhfi <ludhfiansyah@gmail.com>
2025-12-12 14:59:44 +0000

    REVISION 3.js to use GLOBAL const to get rid off the  magic number

--- assignments/03-reading-input-from-console/3.js
+++ assignments/03-reading-input-from-console/3.js
@@ -1,11 +1,18 @@
 const readline = require ('readline-sync');
 
+// REVISION - Add GLOBAL const variable
+const SECONDS_PER_MINUTES = 60;
+
 // Get user input
 const inputSeconds = +readline.question('Enter seconds: ');
 
 // Parse inputSeconds into minutes and seconds
-const minutes = Math.floor(inputSeconds / 60);
-const remainingSeconds = inputSeconds % 60;
+// const minutes = Math.floor(inputSeconds / SE); - Prev-INCORRECT
+// const remainingSeconds = inputSeconds % 60; - Prev-INCORRECT
+
+// REVISION Code
+const minutes = Math.floor(inputSeconds / SECONDS_PER_MINUTES);
+const remainingSeconds = inputSeconds % SECONDS_PER_MINUTES;
 
 // Print output
-console.log(inputSeconds +' seconds is '+ minutes + ' minutes and ' + remainingSeconds + ' seconds');
+console.log('\n' + inputSeconds +' seconds is '+ minutes + ' minutes and ' + remainingSeconds + ' seconds'); // REV-Add new line \n
